b0c7470b29ee44148e0075ef1b4104b60d49ba51
ofwebdev <nkomorfaruk@gmail.com>
2023-05-04 20:22:10 +0600

    Blog complete

--- src/pages/Blog/BlogDetails.jsx
+++ src/pages/Blog/BlogDetails.jsx
@@ -1,6 +1,7 @@
+import { useEffect, useRef, useState } from "react";
 import { Link, useLoaderData, useParams } from "react-router-dom";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import { useEffect, useState } from "react";
+// import { useReactToPrint } from "react-to-pdf";
 import Loading from "../../components/Loading/Loading";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
@@ -24,10 +25,32 @@ function BlogDetails() {
       <Header />
       <Container className="mt-5">
         <Row>
-          <img src={info.image} alt="" />
-          <Col md={6}></Col>
+          <img
+            className="mb-5"
+            src={info.image}
+            alt=""
+            style={{ borderRadius: "32px" }}
+          />
+          <Col md={{ span: 6, offset: 3 }}>
+            {/* <div ref={componentRef}> */}
+            {info.article.map((item, index) => (
+              <div key={index}>
+                {typeof item === "string" && <h5>{item}</h5>}
+                {typeof item === "object" && (
+                  <img className="w-100 my-4" src={item.img} alt="" />
+                )}
+              </div>
+            ))}
+            {/* </div> */}
+          </Col>
         </Row>
       </Container>
+
+      {/* <div className="text-center">
+        <Button variant="primary" onClick={handlePrint}>
+          Download as PDF
+        </Button>
+      </div> */}
       <Footer />
     </>
   );
