886e0bb6a8958daa41f79ced289747c5a1f48244
ofwebdev <nkomorfaruk@gmail.com>
2023-05-04 20:27:02 +0600

    local.env file add for firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,22 +4,13 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_APIKEY,
-//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-//   projectId: import.meta.env.VITE_PROJECTID,
-//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-//   appId: import.meta.env.VITE_APPID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBVkVa8PcuXoBnnLxRCu7PE15W4YjhAQ1k",
-  authDomain: "recipe-client.firebaseapp.com",
-  projectId: "recipe-client",
-  storageBucket: "recipe-client.appspot.com",
-  messagingSenderId: "791580926749",
-  appId: "1:791580926749:web:1d467acd04f3889583b7f4",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
