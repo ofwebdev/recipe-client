1aea39beb09c08c6e6a1987077cb64d2972a9d50
ofwebdev <nkomorfaruk@gmail.com>
2023-05-26 02:26:05 +0600

    add local storage data

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -13,6 +13,9 @@ import "./Post.scss";
 
 function Post({ recipe }) {
   const [isLoading, setIsLoading] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [favoriteItem, setFavoriteItem] = useState(null);
 
   const shareUrl = "https://recipe-client.web.app/" + recipe._id;
   const shareTitle = recipe.title;
@@ -20,6 +23,41 @@ function Post({ recipe }) {
   useEffect(() => {
     setIsLoading(false);
   }, []);
+
+  useEffect(() => {
+    setIsLoading(false);
+    const storedItems = localStorage.getItem("favoriteItems");
+    if (storedItems) {
+      const favorites = JSON.parse(storedItems);
+      const isRecipeFavorited = favorites.hasOwnProperty(recipe._id);
+      setIsFavorite(isRecipeFavorited);
+      if (isRecipeFavorited) {
+        setFavoriteItem(favorites[recipe._id]);
+      }
+    }
+  }, []);
+
+  const handleClick = () => {
+    if (isFavorite) {
+      const storedItems = localStorage.getItem("favoriteItems");
+      if (storedItems) {
+        const updatedItems = { ...JSON.parse(storedItems) };
+        delete updatedItems[recipe._id];
+        localStorage.setItem("favoriteItems", JSON.stringify(updatedItems));
+        setFavoriteItem(null);
+      }
+      setIsFavorite(false);
+    } else {
+      const storedItems = localStorage.getItem("favoriteItems");
+      const items = storedItems ? JSON.parse(storedItems) : {};
+      const updatedItems = { ...items, [recipe._id]: recipe };
+      localStorage.setItem("favoriteItems", JSON.stringify(updatedItems));
+      setIsFavorite(true);
+      setFavoriteItem(recipe);
+    }
+  };
+
+  console.log(favoriteItem);
 
   if (isLoading) {
     return <Loading />;
@@ -33,7 +71,11 @@ function Post({ recipe }) {
           backgroundImage: `url(${recipe.chef.image})`,
         }}
       >
-        <div className="card-img-overlay d-flex flex-column">
+        <div
+          className="card-img-overlay d-flex flex-column"
+          onMouseEnter={() => setShowButton(true)}
+          onMouseLeave={() => setShowButton(false)}
+        >
           <div className="card-body">
             <small className="card-meta mb-2">Chef Leadership</small>
             <h4 className="card-title mt-0 ">
@@ -49,6 +91,25 @@ function Post({ recipe }) {
             <br />
 
             <Link to={`/recipe/${recipe._id}`}>See More</Link>
+
+            {showButton && (
+              <div
+                className="add-to-fav"
+                style={{ textAlign: "right", display: "block" }}
+              >
+                <FaHeart
+                  size={24}
+                  style={{ color: favoriteItem ? "red" : "white" }}
+                />
+                {favoriteItem && <p>{recipe.chef.name} is a favorite item</p>}
+                <button
+                  onClick={handleClick}
+                  className="btn btn-primary animated-button"
+                >
+                  {isFavorite ? "Remove Favorite" : "Add Favorite"}
+                </button>
+              </div>
+            )}
           </div>
           <div className="card-footer">
             <div className="media" style={{ display: "flex", gap: "10px" }}>
